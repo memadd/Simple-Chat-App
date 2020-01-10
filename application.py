e4965ece2e9f433f167d50284d83c86a43bd0930
memadd <memad632@gmail.com>
2020-01-09 19:11:00 -0800

    added my project

---
+++ application.py
@@ -0,0 +1,25 @@
+
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'gjjjgjdftdfgf5656v'
+socketio = SocketIO(app)
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received somthing' + str(json))
+    socketio.emit('my response', json)    
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
